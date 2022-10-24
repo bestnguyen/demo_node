dea847cd638050fd9cb24e0c3e61041d5c41179e
Nguyen Tot <binhuongitc@gmail.com>
2022-10-24 18:34:59 +0700

    add expresss

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express 3' });
+});
+
+router.get('/function1', function(req, res, next) {
+  res.render('function1', { title1: 'Xin chào bạn!' });
+});
+
+router.get('/function2', function(req, res, next) {
+  res.render('function2', { title3: 'Express 4499900',var2 :'demo variable 2' });
+});
+
+module.exports = router;
